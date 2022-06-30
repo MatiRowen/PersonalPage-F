480bef2f50b82da8e4511858944da335eaa4a686
Matias Rowensztein <92543676+MatiRowen@users.noreply.github.com>
2022-06-30 15:39:31 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,12 @@
+
+$(function() {
+$(".text").typed({
+    strings: ["Front End developer", "Diseñador", "Diseñador 3D"],
+    typeSpeed: 50,
+    backSpeed: 20,
+    backDelay: 1500,
+    showCursor: true,
+    loop: true
+  });
+});
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screen:{'perfect-m':'880px'},
+    extend: {},
+  },
+  plugins: [
+  ],
+}
